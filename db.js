adba4404a798516f6cc46afa104ca2a6abea5b84
Vish75-c <vishgola191@gmail.com>
2025-08-28 11:22:40 +0530

    thirdchange

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 //basic template for connecting mongodb database with the node js
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config({
-    path:"./.env"
-})
+dotenv.config();
 const url=process.env.Mongo_URL;
-mongoose.connect("mongodb+srv://vishgola191:Atlas123@cluster0.azzcxju.mongodb.net/",{
+const local_url=process.env.Mongo_URL_Local;
+mongoose.connect(local_url,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 });
 
 const db=mongoose.connection;
 db.on('connected',()=>{
+    //console.log(url);
     console.log("Connected to the server");
 })
 db.on('disconnected',()=>{
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import db from "./db.js";
-
+import { configDotenv } from "dotenv";
 import { urlencoded } from "express";
 import PersonRouter from "./routes/person.js";
 import MenuRouter from "./routes/list.js";
+import dotenv from "dotenv";
+dotenv.config();const PORT=process.env.PORT||3000;
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded());
@@ -11,7 +13,7 @@ app.use(express.urlencoded());
 
 app.use('/menu',MenuRouter);
 app.use('/person',PersonRouter);
-app.listen(3000,function(err){
+app.listen(PORT,function(err){
     if(err)console.log("Server Not running")
     else console.log("Server is Running");
 })
